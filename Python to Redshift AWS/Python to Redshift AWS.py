b48f398d834a8ef8e6da0167e1e29a8e95f813de
YENCHUN-L <43433207+YENCHUN-L@users.noreply.github.com>
2019-08-14 14:27:03 +1000

    Add files via upload

---
+++ Python to Redshift AWS/Python to Redshift AWS.py
@@ -0,0 +1,35 @@
+# Read library
+import psycopg2
+import numpy as np
+import pandas as pd
+
+# Connetion info
+con = psycopg2.connect(
+            host = "xxx",
+            port = "xxx",
+            database = "xxx",
+            user = "xxx",
+            password = "xxx",
+            )
+
+# Connect to database
+cur = con.cursor()
+
+# Query to execute
+cur.execute("SQL query;")
+              
+colnames = [desc[0] for desc in cur.description]
+
+
+# Read query to np.array
+df = pd.DataFrame(np.array(cur.fetchall()))
+df.columns = colnames
+
+del colnames
+# Connetion close
+cur.close() 
+
+# Clear connection info
+con.close()
+
+
