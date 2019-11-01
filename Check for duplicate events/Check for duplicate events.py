21dc18f0819a5f1f23200c9c203d1edeb42b121f
YENCHUN-L <43433207+YENCHUN-L@users.noreply.github.com>
2019-11-01 13:51:59 +1100

    Create Check for duplicate events.py

---
+++ Check for duplicate events/Check for duplicate events.py
@@ -0,0 +1,27 @@
+# Check for duplicates events
+
+# Change current working directory to desktop, file will read or write from or to desktop
+import os
+os.chdir(r"C:\Users\LocalUser\Desktop")
+
+# Desire table
+# event_id, account_id, event, created_date(to seconds data)
+
+# Read library
+import psycopg2
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+
+df['created_date'] = pd.to_datetime(df['created_date'])
+
+# get time difference when opp and status is the same
+# https://stackoverflow.com/questions/48347497/pandas-groupby-diff
+df['diff']=df.groupby(['id','event'])['created_date'].diff()
+
+# change to seconds
+df['diff'] = df['diff'].dt.total_seconds()
+
+# time difference less than 3 sec
+df2 = df[df['diff'] < 1]
