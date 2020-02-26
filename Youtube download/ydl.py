5fcfe889f81194a8b86c7537e7aa3086a6431ea8
YENCHUN-L <43433207+YENCHUN-L@users.noreply.github.com>
2020-02-27 03:50:50 +0800

    Create ydl.py

---
+++ Youtube download/ydl.py
@@ -0,0 +1,13 @@
+# ydl1.py
+from __future__ import unicode_literals
+import youtube_dl
+import os
+
+local = os.path.expanduser(r"~\Downloads")
+os.chdir(local)
+
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/playlist?list=PLqIq1bkXU1VsGtE3bSbu-5FxmbacaGdu7'])
+
+    
