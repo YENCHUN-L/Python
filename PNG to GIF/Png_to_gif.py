fcf0f5e72d7f970c4b2b9d78ba6459e96d42906d
YENCHUN-L <43433207+YENCHUN-L@users.noreply.github.com>
2020-04-24 10:15:21 +0800

    Update Png_to_gif.py

--- PNG to GIF/Png_to_gif.py
+++ PNG to GIF/Png_to_gif.py
@@ -7,4 +7,4 @@ for file_name in os.listdir(png_dir):
     if file_name.endswith('.png'):
         file_path = os.path.join(png_dir, file_name)
         images.append(imageio.imread(file_path))
-imageio.mimsave('C:/Users/yliu10/Desktop/png/0.gif', images, duration=1) #save path and file name, duration = speed in second
+imageio.mimsave('C:/Users/"user"/Desktop/png/0.gif', images, duration=1) #save path and file name, duration = speed in second
